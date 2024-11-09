7e1614f969863a35883b7445b44a7b51442ff43f
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-09 16:03:20 +0530

    Update index.tsx

--- src/modules/layout/components/side-menu/index.tsx
+++ src/modules/layout/components/side-menu/index.tsx
@@ -37,11 +37,13 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
           {({ open, close }) => (
             <>
               <Popover.Button
-  data-testid="nav-menu-button"
-className="relative h-full flex items-center transition-all ease-out duration-200 focus:outline-none bg-gray-600 text-white py-2 px-4 rounded hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"  <div className="flex items-center">
-    Menu <span><BarsThree /></span>
-  </div>
-</Popover.Button>
+                data-testid="nav-menu-button"
+                className="relative h-full flex items-center transition-all ease-out duration-200 focus:outline-none bg-gray-600 text-white py-2 px-4 rounded hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+              >
+                <div className="flex items-center">
+                  Menu <span><BarsThree /></span>
+                </div>
+              </Popover.Button>
 
               <Transition
                 show={open}
