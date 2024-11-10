423335fcb72b01fa27d6eb9d06dd04b07916db88
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-10 10:13:06 +0530

    Update page.tsx

--- src/app/[countryCode]/(main)/products/[handle]/page.tsx
+++ src/app/[countryCode]/(main)/products/[handle]/page.tsx
@@ -3,45 +3,43 @@ import { notFound } from "next/navigation"
 
 import ProductTemplate from "@modules/products/templates"
 import { getRegion, listRegions } from "@lib/data/regions"
-import { getProductByHandle } from "@lib/data/products"
-import { sdk } from "@lib/config"
+import { getProductByHandle, getProductsList } from "@lib/data/products"
 
 type Props = {
   params: { countryCode: string; handle: string }
 }
 
 export async function generateStaticParams() {
-  try {
-    const countryCodes = await listRegions().then((regions) =>
-      regions?.map((r) => r.countries?.map((c) => c.iso_2)).flat()
-    )
+  const countryCodes = await listRegions().then(
+    (regions) =>
+      regions
+        ?.map((r) => r.countries?.map((c) => c.iso_2))
+        .flat()
+        .filter(Boolean) as string[]
+  )
 
-    if (!countryCodes) {
-      return []
-    }
-
-    const { products } = await sdk.store.product.list(
-      { fields: "handle" },
-      { next: { tags: ["products"] } }
-    )
-
-    return countryCodes
-      .map((countryCode) =>
-        products.map((product) => ({
-          countryCode,
-          handle: product.handle,
-        }))
-      )
-      .flat()
-      .filter((param) => param.handle)
-  } catch (error) {
-    console.error(
-      `Failed to generate static paths for product pages: ${
-        error instanceof Error ? error.message : "Unknown error"
-      }.`
-    )
-    return []
+  if (!countryCodes) {
+    return null
   }
+
+  const products = await Promise.all(
+    countryCodes.map((countryCode) => {
+      return getProductsList({ countryCode })
+    })
+  ).then((responses) =>
+    responses.map(({ response }) => response.products).flat()
+  )
+
+  const staticParams = countryCodes
+    ?.map((countryCode) =>
+      products.map((product) => ({
+        countryCode,
+        handle: product.handle,
+      }))
+    )
+    .flat()
+
+  return staticParams
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
