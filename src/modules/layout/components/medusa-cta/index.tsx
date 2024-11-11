d37cca185793184ca6dc5e50966f2c69fcd134f2
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-11 22:54:29 +0530

    Update index.tsx

--- src/modules/layout/components/medusa-cta/index.tsx
+++ src/modules/layout/components/medusa-cta/index.tsx
@@ -6,8 +6,23 @@ import NextJs from "../../../common/icons/nextjs"
 const MedusaCTA = () => {
   return (
     <Text className="flex gap-x-2 txt-compact-small-plus items-center">
-      Encrypted Payments, Secure Peace of Mind.
-    </Text>
+      Encrypted Payments, Secure Peace of Mind. 
+<IconBadge>
+  <Stripe />
+</IconBadge>
+<IconBadge>
+  <Mastercard />
+</IconBadge>
+<IconBadge>
+  <Klarna />
+</IconBadge>
+<IconBadge>
+  <Visa />
+</IconBadge>
+<IconBadge>
+  <VerifiedBadge />
+</IconBadge>
+ </Text>
   )
 }
 
