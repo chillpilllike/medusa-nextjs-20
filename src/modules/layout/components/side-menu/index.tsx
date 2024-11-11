6ac2894dd931268046d9fda51afbb60d8cf8c59b
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-11 12:12:49 +0530

    Update index.tsx

--- src/modules/layout/components/side-menu/index.tsx
+++ src/modules/layout/components/side-menu/index.tsx
@@ -29,7 +29,7 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
               <div className="relative flex h-full">
                 <Popover.Button
                   data-testid="nav-menu-button"
-                  className="relative h-full flex items-center transition-all ease-out duration-200 focus:outline-none hover:text-ui-fg-base"
+                  className="relative h-full flex items-center px-4 py-2 border border-gray-400 bg-gray-600 text-white transition-all ease-out duration-200 focus:outline-none hover:bg-gray-700 hover:border-gray-500"
                 >
                   Menu<BarsThree />
                 </Popover.Button>
