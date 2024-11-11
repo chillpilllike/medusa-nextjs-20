8761fa0a22361cb11e175555d9b6ca2bbacd47b0
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-11 18:19:05 +0530

    Update index.tsx

--- src/modules/layout/templates/nav/index.tsx
+++ src/modules/layout/templates/nav/index.tsx
@@ -28,7 +28,7 @@ export default async function Nav() {
               className="txt-compact-xlarge-plus hover:text-ui-fg-base uppercase"
               data-testid="nav-store-link"
             >
-              SecretGreen
+              Secretgreen
             </LocalizedClientLink>
           </div>
 
@@ -52,7 +52,7 @@ export default async function Nav() {
                 data-testid="nav-account-link"
               >
                 <Button variant="secondary">
-            Account <User /
+            Account <User />
             </Button>
               </LocalizedClientLink>
             </div>
