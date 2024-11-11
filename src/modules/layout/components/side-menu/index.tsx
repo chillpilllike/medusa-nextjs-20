51581395450808b2f3ad470217a1b1d23417d1f2
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-11 15:00:20 +0530

    Update index.tsx

--- src/modules/layout/components/side-menu/index.tsx
+++ src/modules/layout/components/side-menu/index.tsx
@@ -15,6 +15,7 @@ const SideMenuItems = {
   Search: "/search",
   Account: "/account",
   Cart: "/cart",
+  Contact us: "/contact-us",
 }
 
 const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
@@ -29,7 +30,7 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
               <div className="relative flex h-full">
                 <Popover.Button
                   data-testid="nav-menu-button"
-                  className="relative h-full flex items-center transition-all ease-out duration-200 focus:outline-none hover:text-ui-fg-base"
+                  className="inline-flex items-center px-4 py-2 bg-gray-600 border border-gray-400 text-white rounded transition-all duration-200 ease-out focus:outline-none hover:bg-gray-700 hover:border-gray-500"
                 >
                   Menu
                 </Popover.Button>
@@ -91,7 +92,7 @@ const SideMenu = ({ regions }: { regions: HttpTypes.StoreRegion[] | null }) => {
                         />
                       </div>
                       <Text className="flex justify-between txt-compact-small">
-                        © {new Date().getFullYear()} Medusa Store. All rights
+                        © {new Date().getFullYear()} Secretgreen. All rights
                         reserved.
                       </Text>
                     </div>
