16f6a6479a0642c61fc51f121e309b8897845d64
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-12 09:54:13 +0530

    Update index.tsx

--- src/modules/layout/components/medusa-cta/index.tsx
+++ src/modules/layout/components/medusa-cta/index.tsx
@@ -6,24 +6,25 @@ import NextJs from "../../../common/icons/nextjs"
 
 const MedusaCTA = () => {
   return (
-    <Text className="flex gap-x-2 txt-compact-small-plus items-center">
-<IconBadge>
-  <Stripe />
-</IconBadge>
-<IconBadge>
-  <Mastercard />
-</IconBadge>
-<IconBadge>
-  <Visa />
-</IconBadge>
-<IconBadge>
-  <Klarna />
-</IconBadge>
-<IconBadge>
-  <VerifiedBadge />
-</IconBadge>
-- Secure & Encrypted.
- </Text>
+    <div className="flex flex-col items-center">
+  <div className="flex gap-x-2 items-center">
+    <IconBadge>
+      <Stripe />
+    </IconBadge>
+    <IconBadge>
+      <Mastercard />
+    </IconBadge>
+    <IconBadge>
+      <Visa />
+    </IconBadge>
+    <IconBadge>
+      <Klarna />
+    </IconBadge>
+    <IconBadge>
+      <VerifiedBadge />
+    </IconBadge>
+  </div>
+  <Text className="txt-compact-small-plus mt-2">Secure & Encrypted</Text>
   )
 }
 
