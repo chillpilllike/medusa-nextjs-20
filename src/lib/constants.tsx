4b2cedefb5a82bebbe27424b467d8632050bf788
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-25 02:06:14 +0530

    Update constants.tsx

--- src/lib/constants.tsx
+++ src/lib/constants.tsx
@@ -27,7 +27,7 @@ export const paymentInfoMap: Record<
     icon: <PayPal />,
   },
   pp_system_default: {
-    title: "Manual Payment",
+    title: "PayID / Bank Transfer",
     icon: <CreditCard />,
   },
   // Add more payment providers here
