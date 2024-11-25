fb0608942acb7ed8eec92c2e672469ffe04b4611
chillpilllike <34432585+chillpilllike@users.noreply.github.com>
2024-11-25 12:34:16 +0530

    Update page.tsx

--- src/app/[countryCode]/(main)/page.tsx
+++ src/app/[countryCode]/(main)/page.tsx
@@ -6,9 +6,9 @@ import { getCollectionsWithProducts } from "@lib/data/collections"
 import { getRegion } from "@lib/data/regions"
 
 export const metadata: Metadata = {
-  title: "Medusa Next.js Starter Template",
+  title: "Home & Garden Products - secretgreen.com.au",
   description:
-    "A performant frontend ecommerce starter template with Next.js 14 and Medusa.",
+    "Explore Secret Green: Your ultimate destination for premium home and garden products.",
 }
 
 export default async function Home({
